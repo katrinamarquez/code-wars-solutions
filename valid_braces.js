a69f22597c7bde3ab6ee9a42148d63fc8416216a
katrinamarquez <katrina.l.marquez@gmail.com>
2020-12-17 15:58:54 +1000

    update code wars for today

---
+++ valid_braces.js
@@ -0,0 +1,24 @@
+// Test.assertEquals(validBraces( "()" ), true);
+// Test.assertEquals(validBraces( "[(])" ), false);
+
+function validBraces(braces){
+  let opening = ['(', '[', '{'];
+  let closing = [')', ']', '}']; 
+  let arr = [];
+
+  for (let i = 0; i < braces.length; i++) {
+    if (opening.includes(braces[i])) {
+      arr.push(braces[i]);
+    } else {
+      if (closing.indexOf(braces[i]) === opening.indexOf(arr[arr.length - 1])) {
+        arr.pop(); 
+      } else {
+        return false
+      }
+    return arr.length === 0;
+    }
+  }
+};
+
+validBraces("[(])");
+validBraces("()");
